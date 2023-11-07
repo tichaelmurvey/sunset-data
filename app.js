3e180b778944dd5da36c36ad1f3ae5f18964cc1a
Michael Turvey <Michaelturvey97@gmail.com>
2023-11-07 15:18:26 -0500

    ignore

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+var cron = require('node-cron');
+const app = express()
+const port = 3000
+var sunsetData = require('./sunsetData.json');
+getAllSunsets(sunsetData).then(() => {
+    console.log(sunsetData);
+});
+
+cron.schedule('0 0 * * *', () => {    
+    console.log("running task")
+    //get sunset data from database
+    getAllSunsets(sunsetData).then(() => {
+        console.log(sunsetData);
+    });
+  });
+
+
+app.get('/', (req, res) => {
+    res.send(sunsetData);
+  })
+  
+  app.listen(port, () => {
+    console.log(`sunset time app listening on port ${port}`)
+  })
+
+  async function getAllSunsets(sunsetData){
+    for(let i = 0; i < sunsetData.length; i++){
+        let sunsetTime = await getSunsetTime(sunsetData[i].Latitude, sunsetData[i].Longitude);
+        sunsetData[i].SunsetTime = sunsetTime;
+    }
+  }
+
+  async function getSunsetTime(latitude, longitude){
+    let sunsetTime = await fetch(`https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`)
+    .then(response => response.json())
+    .then(data => {
+        return data.results.sunset;
+    })
+    return(sunsetTime);
+}
+
